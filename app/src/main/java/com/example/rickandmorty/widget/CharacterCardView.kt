e82129177778f52aee6033938a4550a2350eb69d
Brunoandroid <null96@outlook.com>
2022-04-06 02:00:06 -0300

    save customview

--- app/src/main/java/com/example/rickandmorty/widget/CharacterCardView.kt
+++ app/src/main/java/com/example/rickandmorty/widget/CharacterCardView.kt
@@ -4,29 +4,21 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import androidx.cardview.widget.CardView
-import com.example.rickandmorty.R
-import com.google.android.material.textview.MaterialTextView
+import com.example.rickandmorty.databinding.CharacterCardViewBinding
 
 class CharacterCardView @JvmOverloads constructor(
     context: Context,
-    attrs: AttributeSet? = null
-) : CardView(context, attrs){
+    attrs: AttributeSet?,
+) : CardView(context, attrs) {
 
-    private val tvName: MaterialTextView
-
-    init {
-        val context = LayoutInflater.from(context)
-            .inflate(R.layout.character_card_view, this, false)
-
-        tvName = context.findViewById(R.id.tvName)
-
-        addView(context)
-    }
+    private val binding: CharacterCardViewBinding = CharacterCardViewBinding.inflate(
+        LayoutInflater.from(context), this, true
+    )
 
     fun setData(
         nameCharacter: String?
     ) {
-        tvName.text = nameCharacter
+        binding.tvName.text = nameCharacter
     }
 
 }
