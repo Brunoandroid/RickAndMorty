d0390407cdd49a7523ff86b33307fb8f3c406a4c
Brunoandroid <null96@outlook.com>
2022-04-06 03:20:41 -0300

    save

--- app/src/main/java/com/example/rickandmorty/screen/character/adapter/CharactersViewHolder.kt
+++ app/src/main/java/com/example/rickandmorty/screen/character/adapter/CharactersViewHolder.kt
@@ -40,7 +40,8 @@ class CharactersViewHolder(
 
         character?.let {
             binding.cvCharacter.setData(
-                nameCharacter = it.name
+                nameCharacter = it.name,
+                imageUrl = it.image
             )
         }
     }
--- app/src/main/java/com/example/rickandmorty/widget/CharacterCardView.kt
+++ app/src/main/java/com/example/rickandmorty/widget/CharacterCardView.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import androidx.cardview.widget.CardView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.example.rickandmorty.databinding.CharacterCardViewBinding
 
 class CharacterCardView @JvmOverloads constructor(
@@ -16,9 +18,15 @@ class CharacterCardView @JvmOverloads constructor(
     )
 
     fun setData(
-        nameCharacter: String?
+        nameCharacter: String?,
+        imageUrl: String?
     ) {
         binding.tvName.text = nameCharacter
+        val imageView = binding.imgCharacter
+        Glide.with(imageView)
+            .load(imageUrl)
+            .transition(DrawableTransitionOptions.withCrossFade())
+            .into(imageView)
     }
 
 }
