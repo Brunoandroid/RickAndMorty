dd730f75f3d829fac178fca9bb6198ce25a7a6e8
Bruno Freitas <bruno.araujo@madeinweb.app>
2024-03-21 22:56:37 -0300

    fix: save test integration

--- app/src/androidTest/java/com/example/rickandmorty/screen/character/CharactersFragmentTest.kt
+++ app/src/androidTest/java/com/example/rickandmorty/screen/character/CharactersFragmentTest.kt
@@ -1,33 +1,27 @@
 package com.example.rickandmorty.screen.character
 
-import androidx.fragment.app.testing.FragmentScenario
-import androidx.fragment.app.testing.launchFragmentInContainer
-import androidx.lifecycle.Lifecycle
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.rickandmorty.R
+import com.example.rickandmorty.launchFragmentInHiltContainer
 import junit.framework.TestCase
 import org.junit.Assert.*
-import org.junit.Before
-import org.junit.runner.RunWith
-import com.example.rickandmorty.R
 import org.junit.Test
+import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class CharactersFragmentTest: TestCase() {
 
-    private lateinit var scenario: FragmentScenario<CharactersFragment>
-
-    @Before
-    fun setup() {
-        scenario = launchFragmentInContainer(themeResId = R.style.ThemeOverlay_AppCompat_Dark)
-        scenario.moveToState(Lifecycle.State.STARTED)
-    }
-
     @Test
     fun testing() {
-        onView(withId(R.id.tvTitle)).perform(click())
+        launchFragmentInHiltContainer<CharactersFragment>()
+        onView(withId(R.id.tvTitle)).check(matches(isDisplayed()))
+        Thread.sleep(10000L)
     }
 
 }
