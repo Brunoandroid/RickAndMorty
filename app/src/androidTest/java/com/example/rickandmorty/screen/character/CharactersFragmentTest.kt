14b67804a1a782ec2a7a599d4929da220cc8cc61
Bruno Freitas <bruno.araujo@madeinweb.app>
2025-10-08 01:16:03 -0300

    test: implemented charactersViewModel test

--- app/src/androidTest/java/com/example/rickandmorty/screen/character/CharactersFragmentTest.kt
+++ app/src/androidTest/java/com/example/rickandmorty/screen/character/CharactersFragmentTest.kt
@@ -1,17 +1,14 @@
 package com.example.rickandmorty.screen.character
 
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.rickandmorty.R
 import com.example.rickandmorty.launchFragmentInHiltContainer
 import com.example.rickandmorty.screen.characters.CharactersFragment
 import junit.framework.TestCase
-import org.junit.Assert.*
 import org.junit.Test
 import org.junit.runner.RunWith
 
