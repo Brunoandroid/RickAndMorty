1a1b7d7dcefb67e14abb587433e1e6557d192e3d
Bruno Freitas <bruno.araujo@madeinweb.app>
2025-10-09 14:33:46 -0300

    fix: upgrade ci

--- app/src/main/java/com/example/rickandmorty/screen/characters/CharactersViewModel.kt
+++ app/src/main/java/com/example/rickandmorty/screen/characters/CharactersViewModel.kt
@@ -1,15 +1,17 @@
 package com.example.rickandmorty.screen.characters
 
-import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.PagingData
 import androidx.paging.cachedIn
 import com.example.rickandmorty.data.model.character.Result
 import com.example.rickandmorty.data.repository.CharactersRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
 
-class CharactersViewModel @ViewModelInject constructor(
+@HiltViewModel
+class CharactersViewModel @Inject constructor(
     private val repository: CharactersRepository
 ) : ViewModel() {
 
